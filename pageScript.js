5ec7bdf3a9122821f2554412acf4cd40e4ece15a
hostman1 <jorgegabrielcapellan.git@gmail.com>
2021-03-12 14:40:13 -0300

    segundo

--- pageScript.js
+++ pageScript.js
@@ -2,5 +2,5 @@ async function searchWeather(){
     var city = document.getElementById("city").value;
     console.log(city);
     const response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=cordoba,argentina&appid=bc4132385698018c1be6ea9bec28afe6",{mode: 'cors'});
-    console.log(response);
+    document.getElementById("change").textContent = response.id;
 }
