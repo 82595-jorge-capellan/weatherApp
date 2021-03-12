a5955fbb20168ff2f96eb838740ecc57e290d5cc
hostman1 <76027180+hostman1@users.noreply.github.com>
2021-03-12 14:54:33 -0300

    Update pageScript.js

--- pageScript.js
+++ pageScript.js
@@ -2,7 +2,7 @@ async function searchWeather(){
     
     var city = document.getElementById("city").value;
     console.log(city);
-    const response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=cordoba,argentina&appid=bc4132385698018c1be6ea9bec28afe6",{mode: 'cors'});
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=cordoba,argentina&appid=bc4132385698018c1be6ea9bec28afe6",{mode: 'cors'});
     console.log(response);
     const information = await response.json();
     console.log(information);
