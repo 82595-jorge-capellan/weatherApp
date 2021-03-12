63303590d1d466b193b53cfe765fde6eb60db753
hostman1 <jorgegabrielcapellan.git@gmail.com>
2021-03-12 14:52:52 -0300

    cuarto

--- pageScript.js
+++ pageScript.js
@@ -1,12 +1,12 @@
 async function searchWeather(){
-    try{
-        var city = document.getElementById("city").value;
+    
+    var city = document.getElementById("city").value;
     console.log(city);
     const response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=cordoba,argentina&appid=bc4132385698018c1be6ea9bec28afe6",{mode: 'cors'});
-    document.getElementById("change").textContent = response.id;
-    }
-    catch(err){
-        console.log(err);
-    }
+    console.log(response);
+    const information = await response.json();
+    console.log(information);
+    document.getElementById("change").textContent = information.id;
+    
     
 }
